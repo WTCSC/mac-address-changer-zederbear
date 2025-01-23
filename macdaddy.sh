d446c07f807134258dcae77d2a561d6fe66a4818
Zedrick Hoover <95325867+zederbear@users.noreply.github.com>
2025-01-22 23:41:32 -0700

    Update macdaddy.sh, removed option g, would take to long to implement

--- macdaddy.sh
+++ macdaddy.sh
@@ -2,13 +2,16 @@
 PERMANENT_ADDRESS_FILE=""
 
 usage() { echo "Usage: $0 [-i <eth0|eth1|...>] [-a <mac-address>] [-r] [-u]"
-echo "Example: $0 -i eth0 -a 00:00:00:00:00:00 -r "
-echo "Example: $0 -i eth0 -a 97:1a:d9:aa:a6:fd"
-echo "-i = interface name"
-echo "-a = mac address"
-echo "-r = randomize the MAC address" 
-echo "-u = undo mac-address change (back to original mac address)"
-echo "If you want to get a random mac-address put -a as 00:00:00:00:00:00" 1>&2; exit 1; }
+echo "    Examples:"
+echo "         $0 -i   eth0 -r"
+echo "         $0 -i enp0s3 -a 97:1a:d9:aa:a6:fd"
+echo "         $0 -i  wlan1 -u"
+echo ""
+echo "    Options:"
+echo "        -i        Network interface           (Always required)"
+echo "        -a        Mac Address                 (Required unless using -r or -u)"
+echo "        -r        Randomizes your mac address (Not required)"
+echo "        -u        Resets your mac address     (Not required)" 1>&2; exit 1; }
 
 save_perm_address() {
     local iface="$1"
@@ -18,18 +21,19 @@ save_perm_address() {
     fi
 }
 
-get_mac_address_on_network() {
-    local iface="$1"
-    SUBNET=$(ip -o -f inet addr show dev "$iface" | awk '{print $4}')
 
-    sudo arp-scan --interface="$iface" "$SUBNET" 2>/dev/null \
-    | awk '/([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}/ {print $1, $2}' \
-    | while read -r ip mac; do
-        vendor="$(curl -s "https://api.macvendors.com/$mac")"
-        echo "$ip   $mac   ${vendor:-UNKNOWN}"
-    done
-    exit 0
-}
+# get_mac_address_on_network() {
+#    local iface="$1"
+#    SUBNET=$(ip -o -f inet addr show dev "$iface" | awk '{print $4}')
+#
+#    sudo arp-scan --interface="$iface" "$SUBNET" 2>/dev/null \
+#    | awk '/([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}/ {print $1, $2}' \
+#    | while read -r ip mac; do
+#        vendor="$(curl -s "https://api.macvendors.com/$mac")"
+#        echo "$ip   $mac   ${vendor:-UNKNOWN}"
+#    done
+#    exit 0
+# }
 
 reset_address() {
     local iface="$1"
@@ -56,7 +60,7 @@ verify_mac_address() {
     echo "${1}" | grep -E "^[0-9A-Fa-f]{2}(:[0-9A-Fa-f]{2}){5}$" >/dev/null 2>&1
 }
 
-while getopts ":i:a:rug" o; do
+while getopts ":i:a:ru" o; do
     case "${o}" in
         i)
             i=${OPTARG}
@@ -74,9 +78,9 @@ while getopts ":i:a:rug" o; do
         u)
             u=true
             ;;
-        g)
-            g=true
-            ;;
+#        g)
+#            g=true
+#            ;;
         *) 
             usage
             ;;
@@ -88,7 +92,7 @@ if [ -z "${i}" ]; then
     usage
 fi
 
-if [ "$g" != true ] && [ "$u" != true ] && [ "$r" != true ]; then
+if [ "$u" != true ] && [ "$r" != true ]; then
     if [ -z "$a" ]; then
         usage
     fi
@@ -99,8 +103,8 @@ if [ "$r" = true ]; then
     sudo ifconfig "${i}" down
     sudo ifconfig "${i}" hw ether "$(random_mac_address)"
     sudo ifconfig "${i}" up
-elif [ "$g" = true ]; then
-    get_mac_address_on_network ${i}
+#elif [ "$g" = true ]; then
+#    get_mac_address_on_network ${i}
 elif [ "$u" = true ]; then
     reset_address "${i}"
 else
@@ -109,6 +113,5 @@ else
     sudo ifconfig "${i}" up
 fi
 
-echo "i = ${i}"
-echo "a = ${a}"
-
+echo "New MAC address: $(cat /sys/class/net/${i}/address)"
+echo "Permanent MAC address: $(cat /tmp/${i}_perm_mac.txt)"
